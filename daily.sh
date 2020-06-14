85d999e2805dd4cf14f5bd199b015c8f594cc6fa
davidromero <d-romero@live.com>
2020-06-14 03:06:17 -0600

    refactoring

--- daily.sh
+++ daily.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#Daily Routine
+# Daily Routine
 
 LINE_DEL='-----------------------------------------'
+ESPACES='             '
 
 # Color for Output -e flag
 WHITE_COLOR='\E[1;37m'
@@ -11,61 +12,80 @@ GREEN_COLOR='\E[0;32m'
 YELLOW_COLOR='\E[1;33m'
 RED_COLOR='\E[0;31m'
 CYAN_COLOR='\E[0;36m'
+PURPLE_COLOR='\E[0;35m'
+
+check_health_status() {
+	curl -s --HEAD $1 | grep -q "200"
+	health_status=$?
+	
+	if [[ health_status -eq 0 ]]; then
+		echo -e $GREEN_COLOR"$ESPACES"'....  GOOD ....'
+
+	else
+		echo -e $RED_COLOR"$ESPACES"'....  BAD ....'
+	fi
+}
+
+print_del_lin(){
+	echo -e $BLUE_COLOR $LINE_DEL
+}
+
 
 echo -e $WHITE_COLOR 'Daily Starting Process'
-echo -e $BLUE_COLOR $LINE_DEL
+print_del_lin
 
 echo -e $WHITE_COLOR 'Starting IDEA'
 if ! [ -x "$(command -v idea)" ]; then
 	echo -e  $RED_COLOR'Error: idea is not installed.'
 else
-	echo ''
+	echo -e $CYAN_COLOR 'Launching idea\n'
 	#idea &
 	#sleep 45s
 fi
 
-echo -e $BLUE_COLOR $LINE_DEL
+print_del_lin
 
 echo -e $WHITE_COLOR 'Staring Web Browser'
 if ! [ -x "$(command -v firefox)" ]; then
 	echo -e $RED_COLOR 'Error: firefox is not installed.'
 else
 	#firefox mail.google.com github.com twitter.com
-	echo -e $CYAN_COLOR 'Launching firefox'
+	echo -e $CYAN_COLOR 'Launching firefox\n'
 fi
 
-echo -e $BLUE_COLOR $LINE_DEL
+print_del_lin
 
-echo -e $WHITE_COLOR 'Starting BookStore'
-# calibre &
-echo -e $BLUE_COLOR $LINE_DEL
+echo -e $WHITE_COLOR 'Starting BookShelf'
+if ! [ -x "$(command -v calibre)" ]; then
+	echo -e $RED_COLOR 'Error: calibre is not installed.'
+else
+	echo -e $CYAN_COLOR 'Launching calibre\n'
+	#calibre &
+fi
+
+print_del_lin
 
 echo -e $WHITE_COLOR '**** Checking Website Health Status  ****'
 
-curl -s --HEAD dromero.xyz | grep -q "200"
-health_status=$?
+echo -e $CYAN_COLOR"$ESPACES"'Personal'
 
-if [[ health_status -eq 0 ]]; then
-	echo -e $GREEN_COLOR '....  GOOD .... personal'
-else
-	echo -e $RED_COLOR ' ....  BAD .... personal'
-fi
+check_health_status dromero.xyz
 
-curl -s --HEAD raspberry.dromero.xyz | grep -q "200"
-health_status=$?
+echo -e $CYAN_COLOR"$ESPACES"'Raspberry'
 
-if [[ health_status -eq 0 ]]; then
-	echo -e $GREEN_COLOR '....  GOOD .... raspberry'
-else
-	echo -e $RED_COLOR '....  BAD .... raspberry'
-fi
+check_health_status raspberry.dromero.xyz
 
+print_del_lin
 
-echo -e $BLUE_COLOR $LINE_DEL
-
-echo -e '\E[0;37m Starting SubRoutines'
+echo -e $PURPLE_COLOR '>>>> Starting SubRoutines'
 # TODO other subroutines
 #gnome-terminal
 
-echo -e $WHITE_COLOR 'testing'
+echo -e $CYAN_COLOR 'Conecting DB Server ...\n'
+# launch db_server.sh
+#  TODO get PID to manting status or check services!!
+echo -e $CYAN_COLOR 'Conecting Java Application Server ...\n'
+# launch java_app_server.sh
+
+#echo -e $WHITE_COLOR 'testing'
 
