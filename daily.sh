78dddf9ff2886492289195d9fdeed389c94d676d
davidromero <d-romero@live.com>
2020-06-14 02:09:09 -0600

    check if executable is installed, TODO functions

--- daily.sh
+++ daily.sh
@@ -2,37 +2,70 @@
 
 #Daily Routine
 
-LINE_DEL="-----------------------------------------"
+LINE_DEL='-----------------------------------------'
 
-echo "Daily Starting Process"
-echo $LINE_DEL
+# Color for Output -e flag
+WHITE_COLOR='\E[1;37m'
+BLUE_COLOR='\E[0;34m'
+GREEN_COLOR='\E[0;32m'
+YELLOW_COLOR='\E[1;33m'
+RED_COLOR='\E[0;31m'
+CYAN_COLOR='\E[0;36m'
 
-echo "Starting IDEA"
-# idea &
-# sleep 45s
-echo $LINE_DEL
+echo -e $WHITE_COLOR 'Daily Starting Process'
+echo -e $BLUE_COLOR $LINE_DEL
 
-echo "Staring Web Browser"
-# firefox mail.google.com github.com twitter.com
-echo $LINE_DEL
+echo -e $WHITE_COLOR 'Starting IDEA'
+if ! [ -x "$(command -v idea)" ]; then
+	echo -e  $RED_COLOR'Error: idea is not installed.'
+else
+	echo ''
+	#idea &
+	#sleep 45s
+fi
 
-echo "Starting BookStore"
+echo -e $BLUE_COLOR $LINE_DEL
+
+echo -e $WHITE_COLOR 'Staring Web Browser'
+if ! [ -x "$(command -v firefox)" ]; then
+	echo -e $RED_COLOR 'Error: firefox is not installed.'
+else
+	#firefox mail.google.com github.com twitter.com
+	echo -e $CYAN_COLOR 'Launching firefox'
+fi
+
+echo -e $BLUE_COLOR $LINE_DEL
+
+echo -e $WHITE_COLOR 'Starting BookStore'
 # calibre &
-echo $LINE_DEL
+echo -e $BLUE_COLOR $LINE_DEL
 
-echo "**** Checking Website Health Status  ****"
+echo -e $WHITE_COLOR '**** Checking Website Health Status  ****'
 
 curl -s --HEAD dromero.xyz | grep -q "200"
 health_status=$?
 
 if [[ health_status -eq 0 ]]; then
-	echo -e '\E[0;32m ....  GOOD ....'
+	echo -e $GREEN_COLOR '....  GOOD .... personal'
 else
-	echo -e '\E[0;31m ....  BAD ....'
+	echo -e $RED_COLOR ' ....  BAD .... personal'
 fi
-echo $LINE_DEL
 
-echo "Starting SubRoutines"
+curl -s --HEAD raspberry.dromero.xyz | grep -q "200"
+health_status=$?
+
+if [[ health_status -eq 0 ]]; then
+	echo -e $GREEN_COLOR '....  GOOD .... raspberry'
+else
+	echo -e $RED_COLOR '....  BAD .... raspberry'
+fi
+
+
+echo -e $BLUE_COLOR $LINE_DEL
+
+echo -e '\E[0;37m Starting SubRoutines'
 # TODO other subroutines
 #gnome-terminal
+
+echo -e $WHITE_COLOR 'testing'
 
