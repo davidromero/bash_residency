66d068a3cd34bd3e4decad632316dc4dae0449e5
davidromero <d-romero@live.com>
2020-06-14 00:57:05 -0600

    daily main master script added

--- daily.sh
+++ daily.sh
@@ -2,5 +2,37 @@
 
 #Daily Routine
 
+LINE_DEL="-----------------------------------------"
 
-echo "Hello MF!"
+echo "Daily Starting Process"
+echo $LINE_DEL
+
+echo "Starting IDEA"
+# idea &
+# sleep 45s
+echo $LINE_DEL
+
+echo "Staring Web Browser"
+# firefox mail.google.com github.com twitter.com
+echo $LINE_DEL
+
+echo "Starting BookStore"
+# calibre &
+echo $LINE_DEL
+
+echo "**** Checking Website Health Status  ****"
+
+curl -s --HEAD dromero.xyz | grep -q "200"
+health_status=$?
+
+if [[ health_status -eq 0 ]]; then
+	echo -e '\E[0;32m ....  GOOD ....'
+else
+	echo -e '\E[0;31m ....  BAD ....'
+fi
+echo $LINE_DEL
+
+echo "Starting SubRoutines"
+# TODO other subroutines
+#gnome-terminal
+
